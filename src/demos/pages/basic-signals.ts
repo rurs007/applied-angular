a97f99af449c3f86bd0f3dca2fb71c69d3d5072c
Rakib Sikder <urrakib1956@gmail.com>
2025-08-18 20:13:54 +0000

    components

---
+++ src/demos/pages/basic-signals.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-demos-basic-signals',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>Basic Signals</p> `,
+  styles: ``,
+})
+export class BasicSignals {}
